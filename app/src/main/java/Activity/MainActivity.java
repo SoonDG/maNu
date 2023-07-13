e7874c265f573cdaa3b4a338671b67be042d453d
SoonDG <sans12@naver.com>
2023-07-13 21:47:35 +0900

    commit

--- app/src/main/java/Activity/MainActivity.java
+++ app/src/main/java/Activity/MainActivity.java
@@ -1,7 +1,10 @@
 package Activity;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -11,6 +14,7 @@ import android.view.MenuItem;
 import com.example.my_first_project.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
 
 import Fragment.MainFragment;
 import Fragment.MyMenuFragmet;
@@ -27,7 +31,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         fragmentManager.beginTransaction().replace(R.id.frame_layout, mainFragment).commit(); //시작 화면을 mainFragment로
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar); //툴바 설정
+
+        DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer, R.string.app_name, R.string.app_name);
+        drawer.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        NavigationView navigationView = (NavigationView)findViewById(R.id.menu_navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {//사이드 바 메뉴 클릭 이벤트
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+               int id = item.getItemId();
+               if(id == R.id.account){
+
+               }
+               else if(id == R.id.logout){
+
+               }
+               return true;
+            }
+        });
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_menu_navigation);
         bottomNavigationView.setSelectedItemId(R.id.menu_home); //처음 화면에 맞춰 처음엔 홈버튼이 선택되도록 설정
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() { //하단 메뉴 클릭 이벤트
@@ -47,5 +76,7 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
     }
+
 }

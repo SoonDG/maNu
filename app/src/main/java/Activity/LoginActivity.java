2f81c203a18c0e163bd294d6cd28ddb807b0164d
SoonDG <sans12@naver.com>
2023-07-15 22:08:13 +0900

    commit

--- app/src/main/java/Activity/LoginActivity.java
+++ app/src/main/java/Activity/LoginActivity.java
@@ -1,4 +1,36 @@
 package Activity;
 
-public class LoginActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.my_first_project.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button main_btn = (Button)findViewById(R.id.imsi_btn); //로그인 기능 만들기 전까지 메인 화면 전환용 임시 버튼
+        Button to_reg_btn = (Button) findViewById(R.id.to_reg_btn); //회원가입 버튼
+        main_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        to_reg_btn.setOnClickListener(new View.OnClickListener() { //회원가입 화면으로 전환
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
--- app/src/main/java/Activity/RegisterActivity.java
+++ app/src/main/java/Activity/RegisterActivity.java
@@ -3,8 +3,12 @@ package Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.my_first_project.R;
+
+import java.util.ArrayList;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -12,5 +16,15 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Spinner age_spi = (Spinner) findViewById(R.id.spinner);
+        Spinner gen_spi = (Spinner)findViewById(R.id.spinner3);
+        String [] age_Data = getResources().getStringArray(R.array.age);
+        String [] gen_Data = getResources().getStringArray(R.array.gender);
+        ArrayAdapter ageAdapter = new ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, age_Data);
+        ArrayAdapter genderAdapter = new ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, gen_Data);
+
+        age_spi.setAdapter(ageAdapter);
+        gen_spi.setAdapter(genderAdapter);
     }
 }
